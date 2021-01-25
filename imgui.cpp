4d40d7076eb10b74eabd365a05af9afaff5e6ffa
thedmd <michcic@gmail.com>
2021-01-25 20:37:35 +0100

    WIP. Crude dynamic font texture update + demo.

--- imgui.cpp
+++ imgui.cpp
@@ -4248,6 +4248,12 @@ ImDrawData* ImGui::GetDrawData()
     return viewport->DrawDataP.Valid ? &viewport->DrawDataP : NULL;
 }
 
+ImTextureUpdateData ImGui::GetTextureUpdateData()
+{
+    ImGuiContext& g = *GImGui;
+    return g.IO.Fonts->GetTextureUpdateData();
+}
+
 double ImGui::GetTime()
 {
     return GImGui->Time;
@@ -4546,6 +4552,7 @@ void ImGui::NewFrame()
 
     // Setup current font and draw list shared data
     g.IO.Fonts->Locked = true;
+    g.IO.Fonts->ClearTransientTextures();
     SetCurrentFont(GetDefaultFont());
     IM_ASSERT(g.Font->IsLoaded());
     ImRect virtual_space(FLT_MAX, FLT_MAX, -FLT_MAX, -FLT_MAX);
